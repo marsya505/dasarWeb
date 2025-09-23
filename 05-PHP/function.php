bba9140d437b572cd47d782eaa7dfe6c9b5ee95b
marsya505 <marsyazulfazahrani0@gmail.com>
2025-09-23 12:06:58 +0700

    PHP

---
+++ 05-PHP/function.php
@@ -0,0 +1,12 @@
+<?php
+
+function perkenalan() {
+    echo "Assalamualaikum, ";
+    echo "Perkenalan, nama saya Elok<br/>";
+    echo "Senang berkenalan dengan Anda<br/>";
+}
+
+//memanggil fungsi yang sudah dibuat
+perkenalan();
+
+?>
