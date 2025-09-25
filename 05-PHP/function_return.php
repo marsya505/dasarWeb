242c6d43f596e9eae56cd2e516ab2ccfb68bcb15
marsya505 <marsyazulfazahrani0@gmail.com>
2025-09-25 20:41:14 +0700

    PHP

--- 05-PHP/function_return.php
+++ 05-PHP/function_return.php
@@ -4,15 +4,8 @@ function hitungUmur($thn_lahir, $thn_sekarang) {
     $umur = $thn_sekarang - $thn_lahir;
     return $umur;
 }
-function perkenalan($nama, $salam="Assalamualaikum") {
-    echo $salam.",";
-    echo "Perkenalan, nama saya ".$nama."<br/>";
 
-    //memanggil fungsi lain
-    echo "Saya berusia ". hitungUmur(1988, 2023) ." tahun<br/>";
-    echo "Senang berkenalan dengan anda<br/>";
-}
+echo "Umur saya adalah ". hitungUmur(2005, 2025) ."tahun"
+//isi sesuai tahun lahir kalian
 
-//memanggil fungsi perkenalan
-perkenalan ("Elok");
 ?>
